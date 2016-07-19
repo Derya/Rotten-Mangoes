8e344615c0105eb09fd7547c3df4396223f7e007
Derya Aydede <derya.aydede@gmail.com>
2016-07-19 10:33:37 -0700

    part 10

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,16 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path, notice: "#{@movie.title} was submitted successfully!"
+    else
+      render :new
+    end
+  end
+
   def edit
     @movie = Movie.find(params[:id])
   end
