dd61b30a08109a19a51de79a87e1e98f8476eb04
Derya Aydede <derya.aydede@gmail.com>
2016-07-19 16:03:33 -0700

    added admin column to user database and added link to google when user logged in is an admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719203549) do
+ActiveRecord::Schema.define(version: 20160719214947) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160719203549) do
     t.datetime "updated_at",      null: false
     t.string   "firstname"
     t.string   "lastname"
+    t.boolean  "admin"
   end
 
 end
