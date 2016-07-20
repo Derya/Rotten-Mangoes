438ca9edc5ad0efadadc8d5723a4d2c8b07633fe
Derya Aydede <derya.aydede@gmail.com>
2016-07-19 18:08:05 -0700

    admins can create, edit, and delete users. everything is working but there are problems. for one, it seems i am using the regular user route for admin creating new user somehow? a non admin can send a custom http request or something including the admin=true on a new account signup to make a new account that is an admin. so, that kinda sucks i guess. even apart from that the admin route should be different but i can't figure out the form_for method.

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,43 @@
+class Admin::UsersController < ApplicationController
+
+  before_filter :restrict_to_admin
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :admin, :password)
+  end
+
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def restrict_to_admin
+    if !current_user || !current_user.admin
+      flash[:alert] = "You must log in as an admin."
+      redirect_to new_session_path
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
